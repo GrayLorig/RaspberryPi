571286289e7a206c49f93f4afc7f3811e9e11a32
Gray Lorig <gray@lorig.org>
2013-01-14 20:14:00 -0500

    Add existing drivers to git

---
+++ utilities/chips/MCP23008.h
@@ -0,0 +1,386 @@
+// i2c expander library - slow I/O!
+// also works with the MCP23009
+
+// Code by Adafruit Industries/Limor Fried
+// Adapted for RaspberryPi by Gray Lorig
+// License: LGPL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <unistd.h>
+
+class MCP23008
+{
+public:
+   static const uint8_t INPUT    = 0;
+   static const uint8_t OUTPUT   = 1;
+   static const uint8_t LOW      = 0;
+   static const uint8_t HIGH     = 1;
+   static const uint8_t PULLDOWN = 0;
+   static const uint8_t PULLUP   = 1;
+
+   bool begin(const char *device_name, uint8_t addr);
+   bool begin() { return begin("/dev/i2c-1", 0); }
+   void end();
+   bool isOpen() { return deviceFd_ >= 0; }
+   int  getFd()  { return deviceFd_; }
+
+   bool setupPins(uint8_t iodir, uint8_t pullup = 0, uint8_t invert = 0);
+   bool writePins(uint8_t bits);
+   bool readPins(uint8_t &bits);
+
+   bool pinMode(uint8_t p, uint8_t d);
+   bool digitalWrite(uint8_t p, uint8_t d);
+   bool pullUp(uint8_t p, uint8_t d);
+   uint8_t digitalRead(uint8_t p);
+
+   MCP23008()
+   {
+      i2caddr_ = 0;
+      deviceFd_ = -1;
+      iodir_ = 0xff; // All inputs
+      gppu_ = 0x00;  // No pullup
+      olat_ = 0x00;  // Output latches
+   }
+
+private:
+   static const uint8_t MCP23008_ADDRESS = 0x20;
+   static const uint8_t MCP23008_IODIR   = 0x00;
+   static const uint8_t MCP23008_IPOL    = 0x01;
+   static const uint8_t MCP23008_GPINTEN = 0x02;
+   static const uint8_t MCP23008_DEFVAL  = 0x03;
+   static const uint8_t MCP23008_INTCON  = 0x04;
+   static const uint8_t MCP23008_IOCON   = 0x05;
+   static const uint8_t MCP23008_GPPU    = 0x06;
+   static const uint8_t MCP23008_INTF    = 0x07;
+   static const uint8_t MCP23008_INTCAP  = 0x08;
+   static const uint8_t MCP23008_GPIO    = 0x09;
+   static const uint8_t MCP23008_OLAT    = 0x0A;
+
+   uint8_t i2caddr_;
+   int     deviceFd_;
+   uint8_t iodir_;
+   uint8_t gppu_;
+   uint8_t olat_;
+};
+
+
+//=============================================================================
+// begin: Open the device and initialize it.
+//
+inline bool MCP23008::begin(const char *device, uint8_t addr)
+{
+//
+// Remember chip only supports three bits of addressing
+  if (addr > 7)
+     i2caddr_ = addr;
+  else
+     i2caddr_ = addr | MCP23008_ADDRESS; // Add fixed portion of address
+
+//
+// Attempt to open socket connection
+   if (deviceFd_ >= 0) // Connection is already open
+   {
+      fputs("MCP23008: Device already open", stderr);
+      return false;
+   }
+
+   if ((deviceFd_ = open(device, O_RDWR)) < 0)
+   {
+      fprintf (stderr, "MCP23008: Unable to open device %s", device);
+      return false;
+   }
+
+//
+// Indicate which slave we intend to talk to
+   if (ioctl(deviceFd_, I2C_SLAVE, i2caddr_) < 0)
+   {
+      end();
+      fprintf(stderr, "MCP23008: Unable to ioctl %s.", device);
+      return false;
+   }
+//
+// Gather in the current state of the device
+   uint8_t buffer[1];
+
+   buffer[0] = MCP23008_IODIR;
+   write(deviceFd_, buffer, 1);
+   read(deviceFd_, &iodir_, 1);
+
+   buffer[0] = MCP23008_GPPU;
+   write(deviceFd_, buffer, 1);
+   read(deviceFd_, &gppu_, 1);
+
+   buffer[0] = MCP23008_OLAT;
+   if (write(deviceFd_, buffer, 1) != 1)
+   {
+      end();
+      fputs("MCP23008: Unable to write register on device.\n", stderr);
+      return false;
+   }
+   if (read(deviceFd_, &olat_, 1) != 1)
+   {
+      end();
+      fputs("MCP23008: Unable to read register from device.\n", stderr);
+      return false;
+   }
+   return true;
+}
+
+//=====================================================================
+// end: Close the device connection
+//
+inline void MCP23008::end()
+{
+   if (deviceFd_ >= 0)
+      close (deviceFd_);
+   deviceFd_ = -1;
+}
+
+//====================================================================
+// setupPins: Initialize the GPIO pins on the device.
+//
+inline bool MCP23008::setupPins(uint8_t iodir, uint8_t pullup, uint8_t invert)
+{
+   uint8_t buffer[2];
+
+   if (deviceFd_ < 0) // Make sure the device is open
+   {
+       fputs ("MCP23008: Device is not open\n", stderr);
+       return false;
+   }
+
+   buffer[0] = MCP23008_IODIR;
+   buffer[1] = iodir_ = ~iodir;
+   write(deviceFd_, buffer, 2);
+
+   buffer[0] = MCP23008_IPOL;
+   buffer[1] = invert;
+   write(deviceFd_, buffer, 2);
+
+   buffer[0] = MCP23008_GPPU;
+   buffer[1] = gppu_ = pullup;
+   if (write(deviceFd_, buffer, 2) != 2)
+   {
+      end();
+      fputs("MCP23008: Unable to write control registers.\n", stderr);
+      return false;
+   }
+   return true;
+}
+
+//====================================================================
+// writePins: Write all output pins in one fell swoop.
+//
+inline bool MCP23008::writePins(uint8_t bits)
+{
+   uint8_t buffer[2];
+
+   if (deviceFd_ < 0) // Make sure the device is open
+   {
+       fputs ("MCP23008: Device is not open\n", stderr);
+       return false;
+   }
+
+   buffer[0] = MCP23008_OLAT;
+   buffer[1] = olat_ = bits;
+   if (write(deviceFd_, buffer, 2) != 2)
+   {
+      end();
+      fputs("MCP23008: Unable to write output latch register.\n", stderr);
+      return false;
+   }
+   return true;
+}
+
+//====================================================================
+// readPins: Read all of the inputs in one go.
+//
+inline bool MCP23008::readPins(uint8_t &bits)
+{
+   uint8_t buffer[1];
+
+   if (deviceFd_ < 0) // Make sure the device is open
+   {
+       fputs ("MCP23008: Device is not open\n", stderr);
+       return false;
+   }
+
+   buffer[0] = MCP23008_GPIO;
+   if (write(deviceFd_, buffer, 1) != 1)
+   {
+      end();
+      fputs("MCP23008: Unable to initiate GPIO read.\n", stderr);
+      return false;
+   }
+   if (read(deviceFd_, &bits, 1) != 1)
+   {
+      end();
+      fputs("MCP23008: Read of GPIO registered failed.\n", stderr);
+      return false;
+   }
+   return true;
+}
+
+//====================================================================
+// pinMode: Set whether a pin is an input or an output.
+//
+inline bool MCP23008::pinMode(uint8_t p, uint8_t d)
+{
+   if (deviceFd_ < 0) // Make sure the device is open
+   {
+       fputs ("MCP23008: Device is not open\n", stderr);
+       return false;
+   }
+
+   if (p > 7) // We only have 8 pins
+   {
+      fputs ("MCP23008: Invalid pin specified\n", stderr);
+      return false;
+   }
+
+//
+// Set the direction
+   if (d == MCP23008::INPUT)
+     iodir_ |= 1 << p; 
+   else
+     iodir_ &= ~(1 << p);
+
+// write the new IODIR
+   uint8_t buffer[2];
+
+   buffer[0] = MCP23008::MCP23008_IODIR;
+   buffer[1] = iodir_;
+
+   if (write(deviceFd_, buffer, 2) != 2)
+   {
+      end();
+      fputs("MCP23008: Unable to write iodir register\n", stderr);
+      return false;
+   }
+
+   return true;
+}
+
+//==============================================================
+// digitalWrite: Set the state of an output pin
+//
+inline bool MCP23008::digitalWrite(uint8_t p, uint8_t d)
+{
+   if (deviceFd_ < 0) // Make sure the device is open
+   {
+       fputs ("MCP23008: Device is not open\n", stderr);
+       return false;
+   }
+
+   if (p > 7) // We only have 8 pins
+   {
+      fputs ("MCP23008: Invalid pin specified\n", stderr);
+      return false;
+   }
+//
+// Set the appropriate pin  
+   if (d == MCP23008::HIGH)
+      olat_ |= 1 << p; 
+   else
+      olat_ &= ~(1 << p);
+
+// write the new output latch values
+   uint8_t buffer[2];
+
+   buffer[0] = MCP23008::MCP23008_OLAT;
+   buffer[1] = olat_;
+
+   if (write(deviceFd_, buffer, 2) != 2)
+   {
+      end();
+      fputs("MCP23008: Unable to write olat\n", stderr);
+      return false;
+   }
+
+   return true;
+}
+
+//============================================================
+// pullUp: Set the pullup resister on a pin
+//
+inline bool MCP23008::pullUp(uint8_t p, uint8_t d)
+{
+   if (deviceFd_ < 0) // Make sure the device is open
+   {
+       fputs ("MCP23008: Device is not open\n", stderr);
+       return false;
+   }
+
+   if (p > 7) // We only have 8 pins
+   {
+      fputs ("MCP23008: Invalid pin specified\n", stderr);
+      return false;
+   }
+  
+// Set the pullup state
+   if (d == MCP23008::PULLUP)
+      gppu_ |= 1 << p; 
+   else
+      gppu_ &= ~(1 << p);
+
+// write the new pull up state
+   uint8_t buffer[2];
+
+   buffer[0] = MCP23008::MCP23008_GPPU;
+   buffer[1] = gppu_;
+
+   if (write(deviceFd_, buffer, 2) != 2)
+   {
+      end();
+      fputs("MCP23008: Unable to write gppu\n", stderr);
+      return false;
+   }
+
+   return true;
+}
+
+//=========================================================
+// digitalRead: Pull in the value of an input pin
+//
+inline uint8_t MCP23008::digitalRead(uint8_t p)
+{
+   if (deviceFd_ < 0) // Make sure the device is open
+   {
+       fputs ("MCP23008: Device is not open\n", stderr);
+       return 0xff;
+   }
+
+   if (p > 7) // We only have 8 pins
+   {
+      fputs ("MCP23008: Invalid pin specified\n", stderr);
+      return 0xff;
+   }
+
+//
+// Read back the GPIO register
+   uint8_t buffer[1];
+   buffer[0] = MCP23008::MCP23008_GPIO;
+
+   if (write(deviceFd_, buffer, 1) != 1)
+   {
+      end();
+      fputs ("MCP23008: Unable to write gpio\n", stderr);
+      return 0xff;
+   }
+
+   if (read(deviceFd_, buffer, 1) != 1)
+   {
+      end();
+      fputs ("MCP23008: Unable to read back gpio\n", stderr);
+      return 0xff;
+   }
+
+   return (buffer[0] >> p) & 0x01;
+}
---
+++ utilities/chips/MCP4725.h
@@ -0,0 +1,192 @@
+// Support for MCP4725 12-bit DAC
+
+// Code by Powisset Brook/Gray Lorig
+// License: LGPL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <unistd.h>
+
+class MCP4725
+{
+public:
+   static const uint8_t MODE_NORMAL         = 0x00;
+   static const uint8_t MODE_POWERDOWN_1K   = 0x01;
+   static const uint8_t MODE_POWERDOWN_100K = 0x02;
+   static const uint8_t MODE_POWERDOWN_500K = 0x03;
+
+   bool begin(const char *device_name, uint8_t addr);
+   bool begin() { return begin("/dev/i2c-1", 0); }
+   void end();
+   bool isOpen() { return deviceFd_ >= 0; }
+   int  getFd()  { return deviceFd_; }
+
+   bool powerDown(uint8_t mode, bool persist=false);
+   bool setValue(uint16_t value, bool persist=false);
+
+   MCP4725()
+   {
+      i2caddr_ = 0;
+      deviceFd_ = -1;
+   }
+
+private:
+   static const uint8_t MCP4725_ADDRESS      = 0x60;
+   static const uint8_t MCP4725_FAST_WRITE   = 0x00;
+   static const uint8_t MCP4725_DAC_WRITE    = 0x40;
+   static const uint8_t MCP4725_EEPROM_WRITE = 0x60;
+   static const uint16_t MCP4725_MAX_VALUE   = 0x0fff; // We are a 12-bit DAC
+
+   uint8_t i2caddr_;
+   int     deviceFd_;
+};
+
+
+//=============================================================================
+// begin: Open the device and initialize it.
+//
+inline bool MCP4725::begin(const char *device, uint8_t addr)
+{
+//
+// Remember chip only supports three bits of addressing
+  if (addr > 7)
+     i2caddr_ = addr;
+  else
+     i2caddr_ = addr | MCP4725_ADDRESS; // Add fixed portion of address
+
+//
+// Attempt to open socket connection
+   if (deviceFd_ >= 0) // Connection is already open
+   {
+      fputs("MCP4725: Device already open", stderr);
+      return false;
+   }
+
+   if ((deviceFd_ = open(device, O_RDWR)) < 0)
+   {
+      fprintf (stderr, "MCP4725: Unable to open device %s", device);
+      return false;
+   }
+
+//
+// Indicate which slave we intend to talk to
+   if (ioctl(deviceFd_, I2C_SLAVE, i2caddr_) < 0)
+   {
+      end();
+      fprintf(stderr, "MCP4725: Unable to ioctl %s.", device);
+      return false;
+   }
+
+   return true;
+}
+
+//=====================================================================
+// end: Close the device connection
+//
+inline void MCP4725::end()
+{
+   if (deviceFd_ >= 0)
+      close (deviceFd_);
+   deviceFd_ = -1;
+}
+
+//====================================================================
+// powerDown: Set the DAC into powered down state. The output line will
+//            be pulled down to ground using the indicated resistor.
+//
+inline bool MCP4725::powerDown(uint8_t mode, bool persist)
+{
+   uint8_t buffer[3];
+   int size;
+
+   if (deviceFd_ < 0) // Make sure the device is open
+   {
+       fputs ("MCP4725: Device is not open\n", stderr);
+       return false;
+   }
+
+   if (mode != MODE_POWERDOWN_1K &&
+       mode != MODE_POWERDOWN_100K &&
+       mode != MODE_POWERDOWN_500K) // Only supported powerdown modes
+   {
+      fputs ("MCP4725: Unsupported powerdown mode\n", stderr);
+      return false;
+   }
+
+   if (persist)
+   {
+      buffer[0] = MCP4725_EEPROM_WRITE | (mode << 1);
+      buffer[1] = 0x80; // Set to mid scale
+      buffer[2] = 0x00;
+
+      size = 3;
+   }
+   else // If we are not persisting use a fast write
+   {
+      buffer[0] = MCP4725_FAST_WRITE | (mode << 4) | 0x08;
+      buffer[1] = 0;
+
+      size = 2;
+   }
+
+   if (write(deviceFd_, buffer, size) != size)
+   {
+      end();
+      fputs("MCP4725: Unable to write power down command\n", stderr);
+      return false;
+   }
+
+   return true;
+}
+
+//====================================================================
+// setValue: Set the output value as indicated.
+//
+inline bool MCP4725::setValue(uint16_t value, bool persist)
+{
+   uint8_t buffer[3];
+   int size;
+
+   if (deviceFd_ < 0) // Make sure the device is open
+   {
+       fputs ("MCP4725: Device is not open\n", stderr);
+       return false;
+   }
+
+   if (value > MCP4725_MAX_VALUE) // Make sure we are not out of range
+   {
+      fputs ("MCP4725: Value is out of range\n", stderr);
+      return false;
+   }
+
+   if (persist)
+   {
+      buffer[0] = MCP4725_EEPROM_WRITE | (MODE_NORMAL << 1);
+      buffer[1] = (value >> 4) & 0xff; // Top 8 bits
+      buffer[2] = (value << 4) & 0xff; // Bottom 4 bits
+
+      size = 3;
+   }
+   else // If we are not persisting use a fast write
+   {
+      buffer[0] = MCP4725_FAST_WRITE | (MODE_NORMAL << 4) | (value >> 8);
+      buffer[1] = value & 0xff;
+
+      size = 2;
+   }
+
+   if (write(deviceFd_, buffer, size) != size)
+   {
+      end();
+      fputs("MCP4725: Unable to write value command\n", stderr);
+      return false;
+   }
+
+   return true;
+}
